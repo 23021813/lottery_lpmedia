9ab6d82ed4bb5a8e943ef32ee81a3115bd0f8a4f
Hiep Vu <h2mpeg@gmail.com>
2025-08-05 15:32:53 +0700

    Update

--- App.tsx
+++ App.tsx
@@ -83,7 +83,6 @@ export default function App() {
   const renderRegisterView = () => (
     <>
       <div className="text-center mb-10">
-        <h2 className="text-3xl md:text-4xl font-bold text-white drop-shadow-2xl mb-2 uppercase">CHƯƠNG TRÌNH QUAY SỐ MAY MẮN</h2>
       </div>
       <div className="max-w-md mx-auto">
         <RegistrationForm onNewSubmission={handleNewSubmission} agencies={agencies} />
